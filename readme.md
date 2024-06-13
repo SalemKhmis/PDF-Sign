9b36e3b069d3ea71aefbd6a995f02e1c8096d9a0
salemKhmiss <skhmis@affitech.fr>
2024-06-13 19:48:36 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# PDF Sign
+
+## How to use?
+
+1. Click `Choose PDF` to upload a `.pdf` file.
+2. Add images, signatures, text to your PDF.
+3. Click `Save`.
+4. That's it! All is done **in your browser**.
+
+## Features
+
+- Resize and move everything.
+- Add signatures.
+- Adjust line height, font size, font family.
+- Mobile friendly.
+- Drag and drop to upload your PDF.
+---
+
---
+++ src/LeftMenu.svelte
@@ -0,0 +1,68 @@
+<script>
+    export let selectedPageIndex;
+  </script>
+  
+  <style>
+    .left-menu {
+      background-color: #f3f3f3;
+      width: 250px;
+      height: 100%;
+      padding: 20px;
+    }
+    .left-menu h3 {
+      font-size: 1.2rem;
+      font-weight: bold;
+      margin-bottom: 1rem;
+    }
+    .menu-item {
+      display: flex;
+      align-items: center;
+      margin-bottom: 1rem;
+      cursor: pointer;
+    }
+    .menu-item img {
+      margin-right: 0.5rem;
+    }
+  </style>
+  
+  <div class="left-menu">
+    <h3>TillScoSign</h3>
+    <div class="menu-item">
+      <img src="signature_icon.svg" alt="Signature Icon" />
+      <span>Signature</span>
+    </div>
+    <div class="menu-item">
+      <img src="initials_icon.svg" alt="Initials Icon" />
+      <span>Initials</span>
+    </div>
+    <div class="menu-item">
+      <img src="stamp_icon.svg" alt="Stamp Icon" />
+      <span>Stamp</span>
+    </div>
+    <div class="menu-item">
+      <img src="date_signed_icon.svg" alt="Date Signed Icon" />
+      <span>Date Signed</span>
+    </div>
+    <h3>Other Details</h3>
+    <div class="menu-item">
+      <img src="name_icon.svg" alt="Name Icon" />
+      <span>Name</span>
+    </div>
+    <div class="menu-item">
+      <img src="email_icon.svg" alt="Email Icon" />
+      <span>Email</span>
+    </div>
+    <div class="menu-item">
+      <img src="company_icon.svg" alt="Company Icon" />
+      <span>Company</span>
+    </div>
+    <div class="menu-item">
+      <img src="title_icon.svg" alt="Title Icon" />
+      <span>Title</span>
+    </div>
+    <div class="menu-item">
+      <img src="custom_signature_icon.svg" alt="Custom Signature Icon" />
+      <span>Custom Signature</span>
+    </div>
+  </div>
+  
